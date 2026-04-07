726d22fd88958442bb44820e0d949a39b1013f81
HeyItsChloe <chloearibo92@gmail.com>
2026-04-07 08:59:06 -0700

    updating revision

--- enterprise/migrations/versions/107_add_onboarding_completed_to_user.py
+++ enterprise/migrations/versions/107_add_onboarding_completed_to_user.py
@@ -3,8 +3,8 @@
 Tracks whether a user has completed the onboarding flow.
 Used to redirect new SaaS users to /onboarding after accepting TOS.
 
-Revision ID: 105
-Revises: 104
+Revision ID: 107
+Revises: 106
 Create Date: 2026-03-31
 """
 
@@ -14,8 +14,8 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '105'
-down_revision: Union[str, None] = '104'
+revision: str = '107'
+down_revision: Union[str, None] = '106'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
